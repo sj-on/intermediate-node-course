31e8055a23995dacd9e21d34355a971fb671078d
sj-on <samhitajoshi15@gmail.com>
2021-03-04 18:36:32 +0530

    pikachu

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ mongoose.connect('mongodb://localhost/userData')
 const port=8000;
 const app= express();
 
-const User=require('./models/User');
 mongoose.connect('mongodb://localhost/userData')
 
 app.use(bodyParser.json());
@@ -18,13 +17,42 @@ app.listen(port, ()=>{
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
 })
 
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
+app.route('/users/:id').get((req,res)=>{
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
 // UPDATE
 .put((req,res)=>{
