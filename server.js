fc4afbb11a6b405e0906620e9ea6eceebad4e220
sj-on <samhitajoshi15@gmail.com>
2021-04-08 13:09:21 +0530

    second comment XD

--- server.js
+++ server.js
@@ -1,69 +1,7 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const User=require('./models/User');
-mongoose.connect('mongodb://localhost/userData')
+const express = require('express')
+const dotenv = require('dotenv')
 
-const port=8000;
-const app= express();
-
-mongoose.connect('mongodb://localhost/userData')
-
-app.use(bodyParser.json());
-
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
-
-// CREATE
-app.post('/users',(req,res)=>{
-  User.create(
-    {...req.body.newData},
-    (err,data)=>{sendResponse(res,err,data)
-  })
-})
-
-app.route('/users/:id').get((req,res)=>{
-  User.findById(req.params.id,(err,data)=>{sendResponse(res,err,data)
-  })
-})
-// UPDATE
-.put((req,res)=>{
-  User.findByIdAndUpdate(
-    req.params.id,
-    {...req.body.newData},
-    {
-      new:true
-    },
-    (err,data)=>{sendResponse(res,err,data)
-    }
-  )
-})
-// DELETE
-// DELETE
-.delete((req,res)=>{
-  User.findByIdAndDelete(
-    req.params.id,
-    (err,data)=>{sendResponse(res,err,data)
-    }
-  )
-})
-
-function sendResponse(res,err,data){
-  if (err){
-    res.json({
-      success: false,
-      message: err
-    })
-  } else if (!data){
-    res.json({
-      success: false,
-      message: "Not Found"
-    })
-  } else {
-    res.json({
-      success: true,
-      data: data
-    })
-  }
-}
+dotenv.config({ path: './config.env'})
+const app = express()
+const PORT = process.env.PORT || 80;
+app.listen(PORT, console.log(`Running a ${process.env.ENV} server on ${PORT}`))
